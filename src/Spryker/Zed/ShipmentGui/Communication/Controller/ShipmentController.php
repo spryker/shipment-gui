947851497fcb95d578c121f823e4143cffd92937
Sergey Palkin <sergey.palkin@spryker.com>
2019-01-03 12:58:17 +0200

    CC-1786: Move functionality in ShipmentGui module.

---
+++ src/Spryker/Zed/ShipmentGui/Communication/Controller/ShipmentController.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ShipmentGui\Communication\Controller;
+
+use Generated\Shared\Transfer\AddressTransfer;
+use Generated\Shared\Transfer\OrderTransfer;
+use Spryker\Service\UtilText\Model\Url\Url;
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+use Spryker\Zed\Sales\SalesConfig;
+use Symfony\Component\HttpFoundation\Request;
+
+class ShipmentController extends AbstractController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function editAction(Request $request)
+    {
+        $idSalesOrder = $this->castId($request->query->get(SalesConfig::PARAM_ID_SALES_ORDER));
+        $idShipment = $this->castId($request->query->get('id-shipment'));
+
+        $dataProvider = $this->getFactory()->createEditShippingFormDataProvider();
+        $form = $this->getFactory()
+            ->getEditShippingForm(
+                $dataProvider->getData($idShipment),
+                $dataProvider->getOptions()
+            )
+            ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $addressTransfer = (new AddressTransfer())->fromArray($form->getData(), true);
+            $addressTransfer->setIdSalesOrderAddress($idShipment);
+            $this->getFacade()
+                ->updateOrderAddress($addressTransfer, $idShipment);
+
+            $this->addSuccessMessage('Address successfully updated.');
+
+            return $this->redirectResponse(
+                Url::generate(
+                    '/sales/detail',
+                    [
+                        SalesConfig::PARAM_ID_SALES_ORDER => $idSalesOrder,
+                    ]
+                )->build()
+            );
+        }
+
+        return $this->viewResponse([
+            'idSalesOrder' => $idSalesOrder,
+            'form' => $form->createView(),
+            'eventsGroupedByItem' => [],
+        ]);
+    }
+}
---
+++ src/Spryker/Zed/ShipmentGui/Communication/Form/ShipmentForm.php
@@ -0,0 +1,184 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ShipmentGui\Communication\Form;
+
+use Spryker\Zed\Kernel\Communication\Form\AbstractType;
+use Spryker\Zed\Kernel\Communication\Form\FormTypeInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Blank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * @method \Spryker\Zed\Sales\Business\SalesFacadeInterface getFacade()
+ * @method \Spryker\Zed\Sales\Communication\SalesCommunicationFactory getFactory()
+ * @method \Spryker\Zed\Sales\Persistence\SalesQueryContainerInterface getQueryContainer()
+ * @method \Spryker\Zed\Sales\SalesConfig getConfig()
+ * @method \Spryker\Zed\Sales\Persistence\SalesRepositoryInterface getRepository()
+ */
+class ShipmentForm extends AbstractType
+{
+    public const FIELD_ADDRESS_FORM_ID = 'address_form_id';
+    public const FIELD_SHIPMENT_DATE = 'delivery_date';
+    public const FIELD_ORDER_ITEMS_FORM_ID = 'order_items_form_id';
+    public const FIELD_SHIPMENT_METHOD = 'shipment_method';
+    public const FIELD_DELIVERY_ADDRESS = 'delivery_address';
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'address';
+    }
+
+    /**
+     * @deprecated Use `getBlockPrefix()` instead.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     *
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(AddressForm::OPTION_SALUTATION_CHOICES);
+        $resolver->setRequired(AddressForm::OPTION_COUNTRY_CHOICES);
+    }
+
+    /**
+     * @deprecated Use `configureOptions()` instead.
+     *
+     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
+     *
+     * @return void
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $this->configureOptions($resolver);
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     *
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this
+            ->addAddressForm($builder)
+//            ->addOrderItemsForm($builder)
+            ->addShipmentMethodField($builder)
+            ->addDeliveryDateField($builder)
+        ;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    public function addDeliveryAddressField(FormBuilderInterface $builder)
+    {
+        $builder->add(
+            self::FIELD_DELIVERY_ADDRESS,
+            RadioType::class, [
+                'constraints' => [
+                    new Blank(),
+                ],
+            ]
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $choices
+     *
+     * @return $this
+     */
+    protected function addAddressForm(FormBuilderInterface $builder)
+    {
+        $builder->add(
+            self::FIELD_ADDRESS_FORM_ID,
+            AddressForm::class,
+            $builder->getOptions()
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $choices
+     *
+     * @return $this
+     */
+    protected function addOrderItemsForm(FormBuilderInterface $builder)
+    {
+        $builder->add(
+            self::FIELD_ORDER_ITEMS_FORM_ID,
+            $builder->getOptions()
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    public function addShipmentMethodField(FormBuilderInterface $builder)
+    {
+        $builder->add(
+            self::FIELD_SHIPMENT_METHOD,
+            TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    public function addDeliveryDateField(FormBuilderInterface $builder)
+    {
+        $builder->add(
+            self::FIELD_SHIPMENT_DATE,
+            TextType::class, [
+                'constraints' => [
+                ],
+            ]
+        );
+
+        return $this;
+    }
+}
